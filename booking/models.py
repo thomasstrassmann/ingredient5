fe308e75a1bd1f94890265c8e20ae6ac149f144a
thomasstrassmann <thomas_strassmann@freenet.de>
2022-11-25 08:43:36 +0000

    create draft model of booking app

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+TIME_SLOTS = (
+    ("5 PM", "5 PM"),
+    ("6:30 PM", "6:30 PM"),
+    ("8 PM", "8 PM"),
+)
+
+
+class Workshop(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=8, choices=TIME_SLOTS, default="5 PM")
+    registered = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return f"{self.user.username} booked on day {self.day} at {self.time}"
