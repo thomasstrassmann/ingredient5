afc9e487075725685a6bebb2e8ee4c7586de9fe4
thomasstrassmann <thomas_strassmann@freenet.de>
2022-11-25 12:14:46 +0000

    add recipeDetail view, template and url

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -8,3 +8,10 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created')
     template_name = 'index.html'
     paginate_by = 5
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug)
+        
