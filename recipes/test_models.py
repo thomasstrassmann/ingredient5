0f23ffbc2383cc27d62f1aaca99ff827693c9546
thomasstrassmann <thomas_strassmann@freenet.de>
2022-11-26 12:44:39 +0000

    write default test for recipe model

--- recipes/test_models.py
+++ recipes/test_models.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Recipe
 
-# Create your tests here.
+# This is a test to confirm, that the default model generates
+# the right standard values for vegetarian, status and type
+
+
+class TestModels(TestCase):
+
+    def test_default_recipe(self):
+        recipe = Recipe.objects.create(
+            title='test',
+            content='This is a test',
+        )
+        self.assertFalse(recipe.vegetarian)
+        self.assertEqual(recipe.status, 0)
+        self.assertEqual(recipe.type, 0)
