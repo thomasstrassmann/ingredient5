eeeff3b8dcaceaded5ffb24c6fb33963575fce71
thomasstrassmann <thomas_strassmann@freenet.de>
2022-12-03 10:46:53 +0000

    create calendar in booking form

--- booking/forms.py
+++ booking/forms.py
@@ -2,7 +2,14 @@ from .models import Workshop
 from django import forms
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Workshop
         fields = ('day', 'time', 'email')
+        widgets = {
+            'day': DateInput(),
+        }
